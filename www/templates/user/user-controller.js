234a951f9497cc705958be7753e7d2d0ff4c447c
michael graham <mike@leafdrop.co>
2016-08-03 14:32:00 -0500

    updated sign in up

--- www/templates/user/user-controller.js
+++ www/templates/user/user-controller.js
@@ -1,7 +1,22 @@
-appControllers.controller('userCtrl', function ($scope, $rootScope, $mdToast, $mdBottomSheet, $timeout, $stateParams) {
+appControllers.controller('userCtrl', function ($scope, $rootScope, $mdToast, $mdBottomSheet, $timeout, $stateParams, $state) {
 
     $scope.initialForm = function () {
-        // Loading progress.
+        $scope.moveBox = function () {
+            // $("#signupbox").fadeOut();
+            $('#signupbox').animate({
+                    'marginTop' : "+=170px" //moves down
+                });
+
+            $timeout(function() {
+                $state.go('app.signup');
+            }, 500);
+
+            $timeout(function() {
+                $('#signupbox').animate({
+                    'marginTop' : "-=170px" //moves down
+                });
+            }, 1000);
+        }
        
     };
 
